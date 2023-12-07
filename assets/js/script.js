0dedf85bf138fa618de45c6c56748f1c175f641b
Eric P <ericmpeterson04@gmail.com>
2023-12-07 12:34:03 -0700

    Got API call to work, added HTML blocks for loading in API data

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,6 @@ var searchHistoryEl = document.querySelector('#search-history');
 var sample = ["a","e","i","o","u"];
 var fetchButton = document.getElementById('fetch-button');
 var APIKey = '06c97fb0689d512139bb0576c61f8276';
-var city = "Denver";
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
 // Create a button and add text & a link of previously searched cities
 for (var i=0; i<sample.length; i++) {
@@ -12,11 +10,17 @@ for (var i=0; i<sample.length; i++) {
     searchHistoryEl.appendChild(historyItem);
 }
 
+// Load weather data for input city when the 'search' button is pressed
 function getApi() {
-  
-fetch(queryURL);
-console.log('success');
+var city = "Denver";
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
+fetch(queryURL)
+    .then(function (response) {
+     console.log(response.json());
+    });
+    
+    
 };
 
-fetchButton.addEventListener('click',getApi);
+fetchButton.addEventListener('click', getApi);
